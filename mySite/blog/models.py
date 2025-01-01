0b89e9a1f64c409806368851d0e73106b5f50797
uzzi002 <101901963+uzzi002@users.noreply.github.com>
2025-01-01 11:02:01 +0200

    Added comments and docstrings to views.py and models.py

--- mySite/blog/models.py
+++ mySite/blog/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    body = models.TextField()
-    date_created = models.DateTimeField(auto_now_add=True)
-    signature = models.CharField(max_length=140, default='Your default quote or name')
-
-    def __str__(self):
-        return self.title
+    """
+    Represents a blog post in the application.
+    Attributes:
+        title (str): The title of the blog post.
+        content (str): The main content of the blog post.
+        date_posted (datetime): The timestamp when the post was created.
+    """
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
